8b7b17b320f4165c4e33431ef32edd38af48b4ff
rjschroeder <are.jay.schroeder@gmail.com>
2022-09-27 20:56:26 -0500

    began work on login input handling
    work in login.js file, setting up initial variables and functions to be called when user enters name and password to check if they meet length or formatting requirements

---
+++ public/js/login.js
@@ -0,0 +1,19 @@
+//Not sure if this is the right place for this code,
+//can be moved to wherever frontend js is at
+//or wherever the script called after the button press is at
+
+//!! This code is for input handling, not for validation
+
+//this variable will change to the content of html form/textbox
+const enteredUser = "John"
+
+//this variable will change to content of html for password
+const enteredPass = "Pass"
+
+function handleUsername(username) {
+
+}
+
+function handlePassword(password) {
+
+}
