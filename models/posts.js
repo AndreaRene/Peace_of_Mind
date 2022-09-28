d16c43382ec285d056294a92ba6ecf951fd9e7a5
Val Pal <valgonzalez13@yahoo.com>
2022-09-27 21:03:15 -0500

    completed post model. work on comments next

---
+++ models/posts.js
@@ -0,0 +1,43 @@
+const {Model, DataTypes} = require('sequelize');
+const bcrypt = require('bcrypt');
+const sequelize = require('../config/connection');
+
+class Post extends Model {}
+
+Post.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        title: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        content: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        date_stamp: {
+            type: DataTypes.DATEONLY,
+            allowNull: false,
+            defaultValue: DataTypes.NOW
+        },
+        user_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'user',
+                key: 'id'
+            },
+        },
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'post'
+    }
+);
+
+module.exports = Post;
