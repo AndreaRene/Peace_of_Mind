bddc43052d8c4a1d0bf34d35a4009cd215c51ec3
Val Pal <valgonzalez13@yahoo.com>
2022-09-27 20:43:30 -0500

    completed user model. work on posts next

--- models/users.js
+++ models/users.js
@@ -28,5 +28,24 @@ User.init(
             },
         },
     },
-    
-)
+    {
+        hooks: {
+            beforeCreate: async(newUserData) => {
+                newUserData.password = await bcrypt.hash(newUserData.password, 10);
+                return newUserData;
+            },
+            beforeUpdate: async(updatedUserData) => {
+                updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+                return updatedUserData;
+            },
+        
+        },
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'user'
+    }
+);
+
+module.exports = User;
