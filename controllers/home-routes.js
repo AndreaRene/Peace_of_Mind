72fa5281cbae5dcebe782b4cdf9e83b6fd076201
ParksFG <ParkerFGay@gmail.com>
2022-09-28 19:27:26 -0500

    Added Homepage get route

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,5 +1,25 @@
 const router = require('express').Router();
 const { User, Post, Comments } = require('../models');
+const withAuth = require('../utils/auth');
+
+router.get('/', async (req, res) => {
+    try {
+        const postData = await Post.findAll({
+            include: [{
+                model: User,
+                attributes: ['user_name'],
+            },
+        ],
+        });
+        const posts = postData.map((post) => post.get({plain: true}));
+        res.render('all-posts', {
+            posts,
+            logged_in: req.session.logged_in
+        })
+    } catch (error) {
+        res.status(500).json(err.message);
+    }
+});
 
 router.get('/login', (req, res) => {
     if (req.session.logged_in) {
