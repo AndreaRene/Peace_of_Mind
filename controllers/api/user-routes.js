e040990160d58f98e5e19f230a99108379bc95e6
Andrea <andrea@interhact.net>
2022-09-29 20:57:24 -0500

    user login post route complete. work on user logout post route

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -19,3 +19,27 @@ router.post('/', async (req, res) => {
         res.status(500).json(err.message);
     };
 });
+
+router.post('/login', async (req, res) => {
+    try {
+        const validPW = user.checkPassword(req.body.password);
+        const user = await User.findOne({
+            where: {
+                user_name: req.body.user_name,
+            },
+        });
+        if (!user || !validPW) {
+            res.status(400).json({ message: 'Username or password incorrect.' });
+            return;
+        };
+        req.session.save(() => {
+            req.session.user_id = user.id;
+            req.session.user_name = user.user_name;
+            req.session.loggedIn = true;
+
+            res.json({ user, message: 'Login successful.' });
+        });
+    } catch (err) {
+        res.status(400).json({ message: 'Invalid username or password' });
+    };
+});
