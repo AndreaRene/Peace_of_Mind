facd21f79543f2f4c9d4d679158a81dfa1c3f8bf
Andrea <andrea@interhact.net>
2022-10-03 22:20:51 -0500

    added dashboard routing to api index. added dashboard routes file

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -3,7 +3,9 @@ const router = require('express').Router();
 const userRoutes = require('./user-routes.js');
 const postRoutes = require('./post-routes.js');
 const commentRoutes = require('./comment-routes.js');
+const dashboardRoutes = require('./dashboard-routes.js');
 
+router.use('/dashboard', dashboardRoutes);
 router.use('/user', userRoutes);
 router.use('/post', postRoutes);
 router.use('/comment', commentRoutes);
