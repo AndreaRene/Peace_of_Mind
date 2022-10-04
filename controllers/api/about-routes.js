5ae8f923b92aa3dbb132c7f6f8370859cf8dd5a5
Andrea <andrea@interhact.net>
2022-10-03 22:30:18 -0500

    added get about page route

---
+++ controllers/api/about-routes.js
@@ -0,0 +1,11 @@
+const router = require('express').Router();
+
+router.get('/', async (req, res) => {
+    try {
+        res.render('about')
+    } catch (error) {
+        res.status(500).json(err.message);
+    }
+});
+
+module.exports = router;
