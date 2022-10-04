dbac0e4cf19b92ee8c13f518b9f7a84615b63e24
rjschroeder <are.jay.schroeder@gmail.com>
2022-10-03 20:52:54 -0500

    added fetch request in event listener
    new async function to call the fetch request once the login button is pressed. UNFINISHED, NEED ROUTES WRITTEN!!! waiting on handlebars to be finished

--- public/js/login.js
+++ public/js/login.js
@@ -34,3 +34,29 @@ function handlePassword(passwordInput, minLen, maxLen) {
         return false;
     }
 }
+async function newFormHandler(event) {
+    event.preventDefault();
+    const enteredUser = document.querySelector('#USERNAME ID/CLASS HERE').value;
+    const enteredPass = document.querySelector('#PASSWORD ID/CLASS HERE').value;
+    //add other blanks of the form/receiving fetch request here,
+    //also include them in body of fetch
+    const response = await fetch(`/ROUTE OF LOGIN HERE`, {
+      method: 'MRTHOD OF FETCH HERE',
+      body: JSON.stringify({
+        enteredUser,
+        enteredPass,
+        //include here
+      }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+    if (response.ok) {
+        //REPLACE THIS LINE BELOW WITH WHAT TO DO
+        //AFTER SUCCESSFUL LOGIN
+      document.location.replace('/');
+    } else {
+      alert('Failed to login');
+    }
+}
+document.querySelector('NAME OF LOGIN FORM HERE').addEventListener('submit', newFormHandler);
