d63b4f1384a4281599b14365a33ca3a8d8fd92a8
wzrdjavi <jmaster112605@gmail.com>
2022-10-04 19:22:28 -0500

    Update loginsignup.js
    loginsignup code

---
+++ public/js/loginsignup.js
@@ -0,0 +1,41 @@
+const loginFormHandler = async function (event) {
+    event.preventDefault();
+  
+    const usernameEl = document.querySelector('#user_name-input-login');
+    const passwordEl = document.querySelector('#password-input-login');
+  
+    const response = await fetch('/api/user/login', {
+      method: 'POST',
+      body: JSON.stringify({
+        user_name: usernameEl.value,
+        password: passwordEl.value,
+      }),
+      headers: { 'Content-Type': 'application/json' },
+    });
+  
+    if (response.ok) {
+      document.location.replace('/dashboard');
+    } else {
+      alert('Failed to login');
+    }
+  };
+  
+  document
+    .querySelector('.login-form')
+    .addEventListener('submit', loginFormHandler);
+
+    const logout = async Function() {
+      const response = await fetch('api/user/logout', {
+        method: 'POST' ,
+        headers: {'content-Type': 'application/json'},
+      });
+
+      if (response.ok) {
+        document.location.replace('/');
+        alert('logged out!')
+      } else {
+        alert('Failed to logout');
+      }
+    };
+
+    document.querySelector('#logout-link').addEventListener('click', logout);
