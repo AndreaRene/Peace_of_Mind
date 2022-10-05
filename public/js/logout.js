538624619f5caf7bb435fa4c34257a1baba58a71
Mariana Hernandez-Chavez <64447806+Mariana21hch@users.noreply.github.com>
2022-10-04 20:05:17 -0500

    Adding logout.js

---
+++ public/js/logout.js
@@ -0,0 +1,14 @@
+const logout = async () => {
+    const response = await fetch('/api/user/logout', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+    });
+  
+    if (response.ok) {
+      document.location.replace('/');
+    } else {
+      alert(response.statusText);
+    }
+  };
+  
+  document.querySelector('#logout-link').addEventListener('click', logout);
