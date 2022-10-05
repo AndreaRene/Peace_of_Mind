b148079b0022b59649d33555116e964e91de06d9
Andrea <andrea@interhact.net>
2022-10-04 23:41:10 -0500

    work on sign up form

--- public/js/loginsignup.js
+++ public/js/loginsignup.js
@@ -1,41 +1,48 @@
 const loginFormHandler = async function (event) {
-    event.preventDefault();
-  
-    const usernameEl = document.querySelector('#username-input-login');
-    const passwordEl = document.querySelector('#password-input-login');
-  
-    const response = await fetch('/api/user/login', {
-      method: 'POST',
-      body: JSON.stringify({
-        user_name: usernameEl.value,
-        password: passwordEl.value,
-      }),
-      headers: { 'Content-Type': 'application/json' },
-    });
-  
-    if (response.ok) {
-      document.location.replace('/api/dashboard');
-    } else {
-      alert('Failed to login');
-    }
-  };
-  
-  document
-    .querySelector('.login-form')
-    .addEventListener('submit', loginFormHandler);
+  event.preventDefault();
 
-    const logout = async function() {
-      const response = await fetch('api/user/logout', {
-        method: 'POST' ,
-        headers: {'content-Type': 'application/json'},
-      });
+  const usernameEl = document.querySelector('#username-input-login');
+  const passwordEl = document.querySelector('#password-input-login');
 
-      if (response.ok) {
-        document.location.replace('/');
-        alert('logged out!')
-      } else {
-        alert('Failed to logout');
-      }
-    };
+  const response = await fetch('/api/user/login', {
+    method: 'POST',
+    body: JSON.stringify({
+      user_name: usernameEl.value,
+      password: passwordEl.value,
+    }),
+    headers: { 'Content-Type': 'application/json' },
+  });
 
-    document.querySelector('#logout-link').addEventListener('click', logout);
+  if (response.ok) {
+    document.location.replace('/api/dashboard');
+  } else {
+    alert('Failed to login');
+  }
+};
+
+const signupFormHandler = async function (event) {
+  event.preventDefault();
+
+  const user_name = document.querySelector('#username-input-signup').value.trim();
+  const password = document.querySelector('#password-input-signup').value.trim();
+  const response = await fetch('/api/user', {
+    method: 'POST',
+    body: JSON.stringify({
+      user_name, password
+    }),
+    headers: { 'Content-Type': 'application/json' },
+  });
+
+  if (response.ok) {
+    document.location.replace('/dashboard');
+  } else {
+    alert('Failed to sign up');
+  }
+};
+
+document
+  .querySelector('.login-form')
+  .addEventListener('submit', loginFormHandler);
+document
+  .querySelector('.signup-form')
+  .addEventListener('submit', signupFormHandler);
