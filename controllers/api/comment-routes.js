1fcada1b189639ab3519112afcf84c38a2aeee10
ParksFG <ParkerFGay@gmail.com>
2022-10-05 21:14:07 -0500

    Comment out unused route

--- controllers/api/comment-routes.js
+++ controllers/api/comment-routes.js
@@ -2,21 +2,21 @@ const router = require('express').Router();
 const { Comment } = require('../../models/');
 const withAuth = require('../../utils/auth');
 
-// GET comments
-router.get('/', withAuth, async (req, res) => {
-    try {
-        const commentText = await Comment.findAll({
-            include: [User],
-        });
-        const comments = commentText.map((comment) => comment.get({ plain: true }));
-        res.render('single-post', {
-            comments,
-            loggedIn: req.session.loggedIn
-        });
-    } catch (err) {
-        res.status(500).json(err);
-    }
-});
+// GET comments Saving just incase this breaks something
+// router.get('/', withAuth, async (req, res) => {
+//     try {
+//         const commentText = await Comment.findAll({
+//             include: [User],
+//         });
+//         const comments = commentText.map((comment) => comment.get({ plain: true }));
+//         res.render('single-post', {
+//             comments,
+//             loggedIn: req.session.loggedIn
+//         });
+//     } catch (err) {
+//         res.status(500).json(err);
+//     }
+// });
 
 // POST new comment
 router.post('/', withAuth, async (req, res) => {
