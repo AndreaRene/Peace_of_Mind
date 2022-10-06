5b220bfe940efe0f2ed2e75d6e6909260a79e7cb
ParksFG <ParkerFGay@gmail.com>
2022-10-05 21:15:40 -0500

    Changed model variable to stop conflicts

--- models/comments.js
+++ models/comments.js
@@ -11,7 +11,7 @@ Comment.init(
             primaryKey: true,
             autoIncrement: true
         },
-        body: {
+        content: {
             type: DataTypes.STRING,
             allowNull: false
         },
